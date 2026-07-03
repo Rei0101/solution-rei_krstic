216c59ce244865c526d468cf0e33312c7097d597
Rei0101 <62380465+Rei0101@users.noreply.github.com>
2026-07-03 18:27:25 +0200

    chore: add Ticket ORM model

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-
 from src.routers import tickets
 
 app = FastAPI()
---
+++ src/models/ticket.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+from src.db.database import Base
+
+
+class Ticket(Base):
+    __tablename__ = "tickets"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    title: Mapped[str] = mapped_column(String, nullable=False)
+    status: Mapped[str] = mapped_column(String, nullable=False)
+    priority: Mapped[str] = mapped_column(String, nullable=False)
+    assignee: Mapped[str | None] = mapped_column(String, nullable=True)
