29055b837b5a860de185aff4dc9f4434e04220c7
Rei0101 <62380465+Rei0101@users.noreply.github.com>
2026-07-03 23:47:02 +0200

    chore: add exception handling to fetches

--- src/services/dummyjson.py
+++ src/services/dummyjson.py
@@ -3,16 +3,27 @@ import os
 import httpx
 from dotenv import load_dotenv
 
+from src.exceptions import ServiceUnavailableError
+
 load_dotenv()
+
 DUMMY_JSON_URL = os.getenv("DUMMY_JSON_URL")
 
 
 async def fetch_data(endpoint: str):
     async with httpx.AsyncClient() as client:
-        response = await client.get(f"{DUMMY_JSON_URL}/{endpoint}")
-        response.raise_for_status()
-        data = response.json()
-        return data
+        try:
+            response = await client.get(f"{DUMMY_JSON_URL}/{endpoint}")
+            response.raise_for_status()
+            return response.json()
+
+        except httpx.RequestError as e:
+            raise ServiceUnavailableError("External API unreachable") from e
+
+        except httpx.HTTPStatusError as e:
+            raise ServiceUnavailableError(
+                f"External API error: {e.response.status_code}"
+            ) from e
 
 
 async def fetch_todos():
